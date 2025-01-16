10ebcac90f9a50474dd1fc5ab5f50ec6eff641df
Eric Chavet <echavet@gmail.com>
2025-01-16 15:25:21 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,2 +1,15 @@
 #!/bin/sh
-    exec /usr/bin/python3 -m http.server 8080
+    set -e
+
+    # Install Node.js
+    NODE_VERSION=$(jq -r '.options.node_version.default' /data/options.json)
+    curl -fsSL https://deb.nodesource.com/setup_${NODE_VERSION}.x | bash -
+    apt-get install -y nodejs
+
+    # Install Bolt.diy
+    git clone https://github.com/stackblitz-labs/bolt.diy.git /app
+    cd /app
+    npm install
+
+    # Start Bolt.diy
+    npm run dev -- --host 0.0.0.0 --port 5173
